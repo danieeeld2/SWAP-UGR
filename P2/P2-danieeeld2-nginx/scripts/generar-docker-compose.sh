20e3b8fb9995017320524f21f730173238e29be5
danieeeld2 <danieeeld2@gmail.com>
2024-04-05 17:09:56 +0200

    Empzamos P2
    Creado casi todo de nginx

---
+++ P2/P2-danieeeld2-nginx/scripts/generar-docker-compose.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <number_of_instances>"
+    exit 1
+fi
+
+num_instances=$1
+
+cat <<EOF >docker-compose.yml
+version: '2.0'
+
+services:
+EOF
+
+for ((i=1; i<=$num_instances; i++)); do
+    cat <<EOF >>docker-compose.yml
+  web$i:
+    image: danieeeld2-apache-image:p2
+    container_name: web$i
+    privileged: true
+    volumes:
+      - ./web_danieeeld2:/var/www/html
+    networks:
+      red_web:
+        ipv4_address: 192.168.10.$((i+1))
+      red_servicios:
+        ipv4_address: 192.168.20.$((i+1))
+
+EOF
+done
+
+cat <<EOF >>docker-compose.yml
+
+networks:
+  red_web:
+    driver: bridge
+    ipam:
+      config:
+        - subnet: 192.168.10.0/24
+  red_servicios:
+    driver: bridge
+    ipam:
+      config:
+        - subnet: 192.168.20.0/24
+EOF
