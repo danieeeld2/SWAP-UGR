4a4a4f4d64c03bd6a11e8e8a06ebb957a81b2778
danieeeld2 <danieeeld2@gmail.com>
2024-05-23 17:16:13 +0200

    fixed

---
+++ P5/P5-granjaweb/scripts/ordenarIPs.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Eliminar las IPs asignadas a los contenedores en la subred red_web
+for ((i=1; i<=8; i++)); do
+    container_name="web$i"
+    echo "Eliminando IP del contenedor $container_name en la subred red_web"
+    docker network disconnect -f p5-granjaweb_red_web $container_name
+done
+
+# Eliminar las IPs asignadas a los contenedores en la subred red_servicios
+for ((i=1; i<=8; i++)); do
+    container_name="web$i"
+    echo "Eliminando IP del contenedor $container_name en la subred red_servicios"
+    docker network disconnect -f p5-granjaweb_red_servicios $container_name
+done
+
+# Asignar las IPs a los contenedores en la subred red_web
+for ((i=1; i<=8; i++)); do
+    container_name="web$i"
+    container_ip="192.168.10.$((i+1))"
+    echo "Asignando IP $container_ip al contenedor $container_name en la subred red_web"
+    docker network connect --ip $container_ip p5-granjaweb_red_web $container_name
+done
+
+
+# Asignar las IPs a los contenedores en la subred red_servicios
+for ((i=1; i<=8; i++)); do
+    container_name="web$i"
+    container_ip="192.168.20.$((i+1))"
+    echo "Asignando IP $container_ip al contenedor $container_name en la subred red_servicios"
+    docker network connect --ip $container_ip p5-granjaweb_red_servicios $container_name
+done
--- P5/P5-locust/locustfile.py
+++ P5/P5-locust/locustfile.py
@@ -1,10 +1,10 @@
 from locust import HttpUser, TaskSet, task, between
 
-class P5_tuusuariougr(TaskSet):
+class P5_danieeeld2(TaskSet):
     @task
     def load_index(self):
         self.client.get("/index.php", verify=False)
 
 class P5_usuarios(HttpUser):
-    tasks = [P5_tuusuariougr]
+    tasks = [P5_danieeeld2]
     wait_time = between(1, 5)
